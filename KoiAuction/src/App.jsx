c717672d068f6d365a0deb0a90674ca7a73baa4e
ngocthao <thaonnse182709@fpt.edu.vn>
2024-10-16 12:42:15 +0700

    fix login logout & about us

--- KoiAuction/src/App.jsx
+++ KoiAuction/src/App.jsx
@@ -1,8 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import Login from "./components/LoginPage/Login";
-import Register from "./components/LoginPage/Register";
+
 import HomePage from "./pages/home/index.jsx";
 import Header from "./components/header/index.jsx";
 import Footer from "./components/footer/index.jsx";
@@ -14,29 +13,29 @@ import BreederDashboard from "./pages/breeder/breeder-dashboard/index.jsx";
 import StaffDashboard from "./pages/staff/staff-dashboard/index.jsx";
 import StaffResponse from "./pages/staff/manage-response/index.jsx";
 import Bidding from "./pages/bidding/Bidding.jsx";
+import Login from "./pages/LoginPage/Login.jsx";
+import Register from "./pages/LoginPage/Register.jsx";
+import CreateAuction from "./pages/staff/manage-auction/index.jsx";
+import RoomDetail from "./pages/staff/manage-room/index.jsx";
+import Password from "./pages/profile/password/index.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AboutUs from "./pages/about/index.jsx";
+import { AuthProvider } from "./components/AuthContext.jsx";
 
 function App() {
   const router = createBrowserRouter([
-    {
-      path: "/auctions/active/:roomId", // Correct path format for dynamic parameters
-      element: (
-        <>
-          <Header />
-          <Bidding />
-          <Footer />
-        </>
-      ),
-    },
     {
       path: "/",
 
       element: (
         <>
-          <Header />
           <HomePage />
-          <Footer />
         </>
       ),
+    },
+    {
+      path: "/about",
+      element: <AboutUs />,
     },
     {
       path: "/profile",
@@ -49,7 +48,6 @@ function App() {
         </>
       ),
     },
-
     {
       path: "/auctions/active",
 
@@ -67,7 +65,6 @@ function App() {
       element: (
         <>
           <Header />
-
           <Availableaution />
           <Footer />
         </>
@@ -94,7 +91,12 @@ function App() {
     },
     {
       path: "/breeder-dashboard",
-      element: <BreederDashboard />,
+      element: (
+        <ProtectedRoute
+          element={<BreederDashboard />}
+          allowedRoles={["BREEDER"]}
+        />
+      ),
       children: [
         {
           path: "breeder-request",
@@ -104,27 +106,53 @@ function App() {
     },
     {
       path: "/staff-dashboard",
-      element: <StaffDashboard />,
+      element: (
+        <ProtectedRoute element={<StaffDashboard />} allowedRoles={["STAFF"]} />
+      ),
       children: [
         {
           path: "staff-request",
-          element: <StaffResponse />,
+          element: (
+            <ProtectedRoute
+              element={<StaffResponse />}
+              allowedRoles={["STAFF"]}
+            />
+          ),
+        },
+        {
+          path: "create-auction",
+          element: (
+            <ProtectedRoute
+              element={<CreateAuction />}
+              allowedRoles={["STAFF"]}
+            />
+          ),
+        },
+        {
+          path: "create-auction/:auctionId",
+          element: (
+            <ProtectedRoute element={<RoomDetail />} allowedRoles={["STAFF"]} />
+          ),
         },
       ],
     },
     {
-      path: "/dashboard",
-      element: <BreederDashboard />,
-      children: [
-        {
-          path: "create-auction/:auctionId",
-          element: <AuctionDetail />,
-        },
-      ],
+      path: "/auctions/active/:roomId",
+      element: (
+        <>
+          <Header />
+          <Bidding />
+          <Footer />
+        </>
+      ),
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
